c506bf7c5b0ce41937075510d55a84e9cf848d7a
Maher Riyadh <57875037+Maher-RJ@users.noreply.github.com>
2020-03-17 19:09:24 +0100

    Update Graph.java

--- Graph.java
+++ Graph.java
@@ -5,15 +5,25 @@ public class Graph {
         System.out.println(fastestRoute(from, to, 2, 3));
     }
     public static int fastestRoute(int[] from, int[] to, int locationA, int locationB) {
+        //initializing the step = 1
         int count = 1;
+        
+        //checking from 'from' locations
         for(int i = 0; i < from.length; i++) {
+            
+            //if to[i] location is equal to starting location that is locationA
             if(to[i] == locationA) {
+                
+                //it will check to further locations after the i index location and will find the destination
                 for(int j = i+1; j < from.length; j++) {
+                    //increment the step
                     count++;
                     if(to[j] == locationB) {
                         count++;
+                        //if destination location is found
                         break;
                     }
+                    //if another starting location is found it will be initialized to step because fastest route is main goal
                     else if(to[j] == locationA) {
                         count = 1;
                         i = j;
@@ -21,10 +31,12 @@ public class Graph {
                     }
                 }
             }
+            //if steps are greater than 1 stop the program
             if(count > 1) {
                 break;
             }
         }
+        //if step = 1 or step = 0 means no route found the make step = -1
         if(count == 1 || count == 0) {
             count = -1;
         }
